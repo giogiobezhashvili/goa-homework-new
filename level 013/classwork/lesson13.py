0490fedaf5764ff612c56296bad5ca4786976771
giogiobezhashvili <giogiobezhashvili@gmail.com>
2025-10-11 18:45:20 +0400

    first commit

---
+++ level 013/classwork/lesson13.py
@@ -0,0 +1,63 @@
+
+# 1) for loop
+
+
+for i in range(5):  
+    print(i)
+
+
+# 2) while loop
+
+i = 0
+while i < 5:
+    print(i)
+    x += 1  
+
+
+# 3) 
+# indentation არის TAB ან SPACE-ებით შექმნილი დაშორება 
+
+
+# if True:
+#     print()
+    
+
+
+# 4) 
+# % -  გამოიყენება ნაშთის საპოვნელად.
+
+
+print(10 % 3)  
+
+# 5) 
+for i in range(0, 51, 2): 
+    print(i)
+
+
+# 6) 
+for i in range(10):
+    print("გიორგი")
+
+
+# 7) 
+name = 1
+while name <= 67:
+    if name > 3:
+        print(x)
+    x += 1
+
+
+# 8)
+
+counter = 0
+while counter < 10:
+    print("გიორგი")
+    counter += 1  
+
+# 9)
+my_fav = 13  
+user_num = int(input("შეიყვანეთ რიცხვი: "))
+
+while user_num < my_fav:
+    print("შეიყვანე უფრო დიდი რიცხვი")
+    user_num = int(input("შეიყვანეთ რიცხვი კიდევ ერთხელ: "))
