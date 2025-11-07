d87a6f7b8886fcb32284b89e8988b5bbe753e402
giogiobezhashvili <giogiobezhashvili@gmail.com>
2025-11-07 23:37:52 +0400

    second commit

---
+++ level 008/classwork/level 008.py
@@ -0,0 +1,27 @@
+# 1
+name = input("შეიყვანეთ თქვენი სახელი: ")
+print(name)
+
+# 2
+num1 = int(input("შეიყვანეთ პირველი რიცხვი: "))
+num2 = int(input("შეიყვანეთ მეორე რიცხვი: "))
+
+sum = num1 + num2
+
+print(sum)
+
+# 3
+age = input("შეიყვანეთ თქვენი ასაკი: ")
+age = int(age)
+print(type(age))
+
+# 4
+name = "გიორგი"     
+age = 16            
+height = 1.75       
+
+
+print(type(name))
+print(type(age))
+print(type(height))
+
