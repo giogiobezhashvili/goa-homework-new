570f231ae95ce9c0cc478ff35785dff3d76ec0ef
giogiobezhashvili <giogiobezhashvili@gmail.com>
2025-11-24 01:02:52 +0400

    second commit

---
+++ level 016/classwork/level 016.py
@@ -0,0 +1,57 @@
+
+# 1
+
+# for loop — ეს არის ციკლი, რომელიც მეორდება რამდენჯერაც ჩვენ გვინდა
+
+for i in range(5):  
+    print(i)
+
+
+# 2
+  
+# indentation — ეს არის შეწევა (tab ან 4 space),
+
+if True:
+    print("ეს არის indentation-ის მაგალითი")  
+# 3
+for i in range(0, 68):  
+    print(i)
+
+# 4
+for i in range(12, 88):
+    print(i)
+
+
+# 5
+for i in range(4, 99, 2):  
+    print(i)
+
+
+# 6
+for i in range(10):
+    print("გიორგი")
+
+
+
+# 7
+word = input("შეიყვანე სიტყვა: ")
+
+for l in word:  
+    print(l)
+
+
+# 8
+num = int(input("შეიყვანე რიცხვი: "))
+
+for i in range(num): 
+    print(i)
+
+
+
+
+
+
+
+
+
+
---
+++ level 016/homework/level 016.py
@@ -0,0 +1,121 @@
+
+
+
+# 1
+
+print(1939>928)
+print(827>=827)
+print(70==70)
+print(89<90)
+print(80<=92)
+
+
+print(828<38)
+print(837<=83)
+print(837==73678)
+print(9302>94583)
+print(738>=8276)
+
+
+
+
+# 2
+
+# sequancing - თანმიმდევრობით შესრულებული მოქმედებები
+# iteration - განმეორებით შესრულებული მოქმედებები სანამ არ დამაყოფილდება პირობა ან ციკლის მეშვეობით შესრულებული მოქმედება
+# selection - არჩევნის მქონე მოქმედდება რომელიც გამოწვეულია პირობის მიხედვით
+
+
+
+# 3
+
+# ეს არის sequencing-ის მაგალითი რადგან ჯერ 67 ხაზს წაიკითხავს და დაწერს და მერე 68 ხაზს
+print("hi")
+print("evryone")
+
+# 4
+
+# for loop — ეს არის ციკლი, რომელიც მეორდება რამდენჯერაც ჩვენ გვინდა
+
+# 5
+
+# range(start, stop, step) რიცხვებით მინიშნებული რაოდენბა სიდანაც გვინდა დავუწყოთ (start) ,
+# სად გვინდა დავასრულოთ (stop) და 
+# რამდენით უნდაიყოს გამოყოფილი რიცხვი (step) 
+# მაგალითად მარტო ლუწი ან მარტო კენტი
+
+# for loop-ში თითოეულ ამ რიცხვზე ცალკე ატარებს ოპერაციას
+
+# 6
+car = "BMW"
+
+for i in range(10):
+    print(car)
+
+
+
+# 7
+
+surname = "Bejashvili"
+for i in range(100):
+    print(surname)
+
+# 8
+favorite_color = "Blue"
+for i in range(46):
+    print(favorite_color)
+
+# 9
+first_letter = "G"
+for i in range(32):
+    print(first_letter)
+
+
+
+
+# 10
+
+a = input("შეიყვანე პირველი  სიტყვა:")
+b = input("შეიყვანე მეორე  სიტყვა:")
+c = input("შეიყვანე მესამე  სიტყვა:")
+d = int(input("შეიყვანე მეოთხე სიტყვა:"))
+
+sum = a + b + c + str(d)
+print(sum)
+
+
+# 11
+var1 = 10         
+var2 = 3.14        
+var3 = "Hello"    
+var4 = True      
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+
+# 12
+
+n1 = int(input("შეიყვანე პირველი რიცხვი: "))
+n2 = int(input("შეიყვანე მეორე რიცხვი: "))
+n3 = int(input("შეიყვანე მესამე რიცხვი: "))
+n4 = int(input("შეიყვანე მეოთხე რიცხვი: "))
+
+sum_result = n1 + n2 + n3 + n4
+
+print(sum_result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
