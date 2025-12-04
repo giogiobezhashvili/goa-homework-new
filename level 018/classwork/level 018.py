e37b9b5bda45e8c8b27ec4b244ea8b58984b5c08
giogiobezhashvili <giogiobezhashvili@gmail.com>
2025-12-05 00:16:23 +0400

    second commit

---
+++ level 018/classwork/level 018.py
@@ -0,0 +1,68 @@
+
+# 1
+
+for i in range(0, 100):
+    print(i)
+
+
+# 2
+
+for i in range(0, 101):
+    print(i)
+
+# 3
+
+for i in range(10, 21):
+    print(i)
+
+# 4
+
+for i in range(100, 201, 5):
+    print(i)
+
+# 5
+
+for i in range(10,  -1,  -1):
+    print(i)
+
+
+# 6
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+# 7
+
+i = 0
+while i <= 100:
+    print(i)
+    i += 2
+
+# 8
+
+i = 10
+while i >= 0:
+    print(i)
+    # i -= 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 018/homework/level 018.py
@@ -0,0 +1,67 @@
+# 1
+
+for i in range(0, 39): 
+    print(i)
+
+# 2
+
+for i in range(30,40):
+    print(i)
+
+# 3
+
+for i in range(12,121):
+    print(i)
+
+# 4
+
+name= "gio"
+age = 16
+
+for i in range(18):
+    print(name,age)
+    # ან მეორე სახით მეორე პრინტი დაბლა დაუწეროთ და მივუთითოთ age 
+
+# 5
+
+for i in range(10, 101, 3): 
+    print(i)
+
+
+# 6
+fruit="ფორტოხალს"
+
+for i in range(1, 31):  
+    print(i, fruit)
+
+# 7
+
+age=input("შეიყვანე შენი ასაკი:" )
+
+for i in range(13):
+    print(age)
+
+# 8
+
+
+name= "gio"
+i=0
+while i <= 9:
+    print(name)
+    i += 1
+
+
+#  9
+
+
+num= 0 
+
+while num <= 24 :
+    print(num)
+    num += 1
+
+
+# 10 
+
+
+
