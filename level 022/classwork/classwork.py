063d991d5c58aeccb4d6f7c8d78ac5a64127819f
giogiobezhashvili <giogiobezhashvili@gmail.com>
2025-12-15 00:15:35 +0400

    second commit

---
+++ level 022/classwork/classwork.py
@@ -0,0 +1,58 @@
+
+
+# 1
+cars = ["Audi", "BMW", "Mercedes", "Porsche", "Toyota"]
+
+print(cars[0])  
+print(cars[2])  
+
+
+# 2
+
+numbers = [0, 2, 4, 6, 8, 10]
+
+print(numbers[1])  
+print(numbers[3]) 
+
+
+# 3
+
+list1 = ["a", "b", "c", "d", "e"]
+
+print(list1[3])
+# 4
+list1[3] = "გიორგი"
+print(list1)
+
+
+
+# 5
+nums = [5, 10, 15]
+
+sum_result = nums[0] + nums[2]
+print(sum_result)
+
+
+# 6
+names = ["ანა", "ნიკა", "ლუკა", "მარი", "საბა", "თემო", "ეკა"]
+
+names[1] = "გიორგი"   
+names[5] = "დავით"   
+
+print(names)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
