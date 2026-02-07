5e626fd9289fa5954f0ec8cdbfe30f43ba4e3249
giogiobezhashvili <giogiobezhashvili@gmail.com>
2026-02-07 16:43:48 +0400

    second commit

---
+++ level 034/classwork/classwork034.py
@@ -0,0 +1,44 @@
+
+
+
+print("_________1________")
+def greet(name):
+    print(f"Hello World! Hello {name}")
+
+greet("giorgi")
+
+
+
+
+print("_________2________")
+
+def double(num):
+    print(num ** 2)
+
+double(5)
+
+print("_________3________")
+
+
+
+def checkOdd(num):
+    if num % 2 == 0:
+        print("ლუწია")
+    else:
+        print("კენტია")
+
+checkOdd(30)
+
+
+
+print("_________4________")
+
+def BMI(height, weight):
+    print( weight / (height * height))
+
+
+BMI(1.73,60)
+
+
+
+
---
+++ level 036/classwork/level036.py
@@ -0,0 +1,19 @@
+
+def numbers(a,b,c,d,e):
+    sum = (a+b+c+d+e) / 5
+    return sum
+
+print(numbers(4,5,6,7,8))
+
+
+def sayHi(name):
+    return f"გამარჯობა {name}"
+
+
+print(sayHi("giorgi"))
+
+def name_up(name):
+    return name.upper()
+
+print(name_up("giorgi"))
+
