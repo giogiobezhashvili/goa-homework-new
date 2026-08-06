e648052a887c808a4699e8ef2f8867b9af760f54
giogiobezhashvili <giogiobezhashvili@gmail.com>
2026-08-07 02:42:37 +0400

    second commit

---
+++ level 073/classwork/cwlevel073.js
@@ -0,0 +1,45 @@
+//str
+console.log("5")
+console.log("gio")
+console.log("bezhashvili")
+console.log("hello")
+console.log("number")
+
+//number
+
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10)
+
+
+
+console.log(true)
+console.log(false)
+console.log(false)
+console.log(true)
+console.log(true)
+
+
+
+console.log(5 + 7)
+console.log(3 + 9)
+
+
+
+console.log(10 - 9)
+console.log( 8 - 3)
+
+console.log( 9 * 9)
+console.log( 2 *3)
+
+console.log( 2 / 4)
+console.log( 3 / 1.5)
+
+console.log( 5 % 4)
+console.log( 200032 % 2)
+
+
+
+
